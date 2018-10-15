fe7ef38a2b45186d6cb9ecc3815fd1b8f6a16ced
keytears <keytears9@gmail.com>
2018-10-15 17:43:41 +0800

    注册——用户名判断

--- mall/apps/users/views.py
+++ mall/apps/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
 
 # Create your views here.
+
+class RegisterUsernameCountAPIView(APIView):
+    """
+    获取用户名个数
+    GET     /users/usernames/(?P<username>\w{5,20})/count/
+    """
+
+    def get(self, request, username):
+
+        # 通过模型查询，获取用户名个数
+        count = User.objects.filter(username=username).count()
+        # 组织返回数据
+        context = {
+            'count': count,
+            'username': username
+        }
+        return Response(context)
